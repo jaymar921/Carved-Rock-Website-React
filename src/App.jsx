94f0649388429b58fd3f7b71a03ab5bd2bc9736c
JayMar <jayharronabejar@gmail.com>
2022-11-29 09:39:43 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Products from "./Products";
+import {Routes, Route} from 'react-router-dom';
+import Detail from "./Detail";
+import Cart from "./Cart";
+
+export default function App() {
+  const [cart, setCart] = useState();
+
+  function addToCart(id, sku){
+    
+  }
+  return (
+    <>
+      <div className="content">
+        <Header />
+        <main>
+          <Routes>
+            <Route path='/' element={<h1>Welcome to Carved Rock Fitness</h1>} />
+            <Route path='/:category' element={<Products />} />
+            <Route path='/:category/:id' element={<Detail />} />
+            <Route path='/cart' element={<Cart />} />
+          </Routes>
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
+}
